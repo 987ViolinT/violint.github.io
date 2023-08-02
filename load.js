17b6ad0011fb3106ffc4a530d7661257a8fc9f8b
987ViolinT <bryce.hassig@gmail.com>
2023-08-02 16:26:31 -0700

    fixed horrible JavaScript massacre

--- load.js
+++ load.js
@@ -1,20 +1,22 @@
-var i = 0;
-var txt = "Hello, World!";
-var speed = 150;
-
-// typeWriter function - prints "Hello, World!"
-function typeWriter() {
-    setTimeout(typeWriter, 450);
+// Intro sequence for page load
+function introSequence() {
     document.getElementById("typehello").innerHTML = "";
     setTimeout(typeWriter, 450);
+}
+
+// Typewriter function to print "Hello, World!"
+function typeWriter() {
+    var i = 0;
+    var txt = "Hello, World!";
+    var speed = 150;
     if (i < txt.length) {
         document.getElementById("typehello").innerHTML += txt.charAt(i);
         i++;
-        setTimeout(typeWriter, speed);
+        setTimeout(introSequence, speed);
     }
 }
 
-// Load typeWriter function on page start
+// Load introSequence function on page start
 window.addEventListener("load", function() {
-    typeWriter();
+    this.setTimeout(introSequence, 450);
 });

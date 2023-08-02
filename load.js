8af426bdb0212e40207d9c040fba4022529f19cd
987ViolinT <bryce.hassig@gmail.com>
2023-08-02 16:06:08 -0700

    improved introduction

--- load.js
+++ load.js
@@ -4,11 +4,15 @@ var speed = 150;
 
 // typeWriter function - prints "Hello, World!"
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typehello").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    document.getElementById("typehello").innerHTML = "The requested resource was not found on this server."
+    setTimeout(typeWriter, 450)
+    document.getElementById("typehello").innerHTML = ""
+    setTimeout(typeWriter, 450)
+    if (i < txt.length) {
+        document.getElementById("typehello").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
 
 // Load typeWriter function on page start
